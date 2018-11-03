ed3b99be4c61b2f04fdd0d720051c986e6211e43
liuda <1625211623@qq.com>
2018-11-03 15:35:29 +0800

    模型完善

--- new_shangmi/shangmi/models.py
+++ new_shangmi/shangmi/models.py
@@ -39,10 +39,32 @@ class Balance(models.Model):
         verbose_name = "余额积分表"
 
 
+class Advertise(models.Model):
+    name = models.CharField(
+        verbose_name="海报名字",
+        max_length=50
+    )
+    icon = models.CharField(
+        max_length=255,
+        verbose_name="海报封面"
+    )
+    affect_time = models.DateField(
+        verbose_name="截止日期",
+        null=True
+    )
+    class Meta:
+        verbose_name = "海报"
+
+
 class Store(models.Model):
     name = models.CharField(
         verbose_name="店铺名",
         max_length=40
+    )
+    address = models.CharField(
+        verbose_name="地址",
+        max_length=255,
+        null=True
     )
     boss = models.ForeignKey(
         ShangmiUser,
@@ -73,3 +95,50 @@ class GetMoneyLog(models.Model):
     )
     class Meta:
         verbose_name = "用户提现表"
+
+
+class Active(models.Model):
+    name = models.CharField(
+        max_length=40,
+        verbose_name="活动名字"
+    )
+    icon = models.CharField(
+        max_length=255,
+        verbose_name="海报封面"
+    )
+    desc = models.CharField(
+        max_length=255,
+        verbose_name="活动描述"
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name="是否活跃"
+    )
+    create_time = models.DateTimeField(
+        verbose_name="创建时间",
+        auto_now_add=True
+    )
+    give_money = models.IntegerField(
+        verbose_name="活动给予积分"
+    )
+
+    class Meta:
+        verbose_name = "活动表"
+
+
+class ActiveStoreMap(models.Model):
+    active = models.ForeignKey(
+        Active,
+        verbose_name="活动"
+    )
+    store = models.ForeignKey(
+        Store,
+        verbose_name="门店"
+    )
+    create_time = models.DateTimeField(
+        verbose_name="创建时间",
+        auto_now_add=True
+    )
+    class Meta:
+        verbose_name = "活动与门店关系表"
+        unique_together = ['store', 'active'] #联合约束
